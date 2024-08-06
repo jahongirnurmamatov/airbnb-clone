7739c8b58933de9bbf6aa4d6845d315897e739c8
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-06 22:44:05 +0500

    input and heading components done

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,58 @@
+"use client";
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import userRegisterModal from "@/app/hooks/useRegisterModal";
+import { useState } from "react";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+const RegisterModal = () => {
+  const registerModal = userRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
+      <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required/>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RegisterModal;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,8 +4,10 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import userRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
+  const registerModal=userRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -39,7 +41,7 @@ const UserMenu = () => {
             <div className="flex flex-col cursor-pointer">
                 <>
                 <MenuItem onClick={()=>{}} label="Login"/>
-                <MenuItem onClick={()=>{}} label="Sign up"/>
+                <MenuItem onClick={registerModal.onOpen} label="Sign up"/>
                 </>
             </div>
         </div>
