4925be8fab5694ab467033299439a3e6b12324ac
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-06 18:46:43 +0500

    modal for login and its button component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 const inter = Inter({ subsets: ["latin"] });
 const font = Nunito({subsets:["latin"]})
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal actionLabel="Submit" isOpen title="Login "/>
         <Navbar/>
         </ClientOnly>
         {children}
