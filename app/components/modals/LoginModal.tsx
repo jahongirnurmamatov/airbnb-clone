c0f541b7d2539653caf3d78def860f1d11fe82c8
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-07 21:45:48 +0500

    categories with selected property

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -77,13 +77,13 @@ const LoginModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => signIn('google')}
+        onClick={() => signIn('google',{ callbackUrl: '/' })}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => signIn('github')}
+        onClick={() => signIn('github', { callbackUrl: '/' })}
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row items-center gap-2 justify-center">
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     images:{
         domains:["avatars.githubusercontent.com",
-            "https://lh3.googleusercontent.com"
+            "lh3.googleusercontent.com"
         ]
     }
 };
