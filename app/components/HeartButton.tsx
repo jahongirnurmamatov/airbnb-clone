97de018b3b75697dcf464dda680d293ff5d3ccb2
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-13 15:38:55 +0500

    ListingPage done with corresponding  hooks to obtain info

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -19,7 +19,7 @@ const HeartButton:React.FC<HeartButtonProps> = ({
         onClick={toggleFavorite}
         className="relative hover:opacity-80 transition cursor-pointer">
             <AiOutlineHeart size={28} className="fill-white absolute -top-[2px] -right-[2px]" />
-            <AiFillHeart size={24} className={hasFavorited ? 'fill-rose-500':'fill-neutral-500/70'} />
+            <AiFillHeart size={24} className={hasFavorited ? 'fill-rose-500':'fill-neutral-300/90'} />
         </div>
         
     );
