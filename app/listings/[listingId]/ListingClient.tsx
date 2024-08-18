39ee266fd70283b15160340b06fc7e57bf26c858
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-18 15:05:09 +0500

    MyReservationsPage done with cancelling functionality

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -71,6 +71,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
         toast.success("Listing reserved");
         setDateRange(initialDateRange);
         router.push('/trips');
+        router.refresh();
       })
       .catch((err) => {
         toast.error("Something went wrong!");
