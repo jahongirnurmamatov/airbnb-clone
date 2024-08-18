14dd9b7ba61f596e7fc29f86add3847bc5aaa31a
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-18 20:58:59 +0500

    errors fixed

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -28,46 +28,45 @@ export default async function getListings(params: IListingsParams) {
     if (userId) {
       query.userId = userId;
     }
-    if(category){
-        query.category=category;
+    if (category) {
+      query.category = category;
     }
-    if(roomCount){
-        query.roomCount={
-            gte:+roomCount
-        }
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
     }
-    if(guestCount){
-        query.guestCount={
-            gte:+guestCount
-        }
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
     }
-    if(bathroomCount){
-        query.bathroomCount={
-            gte:+bathroomCount
-        }
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
     }
-    if(locationValue){
-        query.locationValue = locationValue;
+    if (locationValue) {
+      query.locationValue = locationValue;
     }
-    if(startDate && endDate){
-        query.NOT ={
-            reservations:{
-                some:{
-                    OR:[
-                        {
-                            endDate:{gte:startDate},
-                            startDate:{lte:startDate}
-                        },
-                        {
-                            startDate: {lte:endDate},
-                            endDate:{gte:endDate}
-                        }
-                    ]
-                }
-            }
-        }
+    if (startDate && endDate) {
+      query.reservations = {
+        none: {
+          OR: [
+            {
+              endDate: { gte: startDate },
+              startDate: { lte: startDate },
+            },
+            {
+              startDate: { lte: endDate },
+              endDate: { gte: endDate },
+            },
+          ],
+        },
+      };
     }
     
+    console.log(JSON.stringify(query, null, 2));
 
     const listings = await prisma.listing.findMany({
       where: query,
@@ -75,6 +74,7 @@ export default async function getListings(params: IListingsParams) {
         createdAt: "desc",
       },
     });
+
     const safeListings = listings.map((listing) => ({
       ...listing,
       createdAt: listing.createdAt.toISOString(),
@@ -82,6 +82,6 @@ export default async function getListings(params: IListingsParams) {
 
     return safeListings;
   } catch (error: any) {
-    throw new Error(error);
+    throw new Error(error.message);
   }
 }
