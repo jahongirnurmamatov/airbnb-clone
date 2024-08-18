e440dc824f421a2773c85434dc9ecd6082d1916c
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-18 19:09:15 +0500

    searchModal ui done with calendar and map functions and params url settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import ToastProvider from "./providers/ToastProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
 
 const inter = Inter({ subsets: ["latin"] });
 const font = Nunito({ subsets: ["latin"] });
@@ -28,6 +29,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToastProvider />
+          <SearchModal />
           <RegisterModal />
           <LoginModal />
           <RentModal />
